a01d70bea76462e42eb2e102117b0e2ce1c4ce5a
ciccio42 <francescorosa97@virgilio.it>
2023-05-03 10:56:54 +0200

    [ADD] Tuned trajectory executor

--- dataset_collector/src/trajectory_collector.py
+++ dataset_collector/src/trajectory_collector.py
@@ -6,7 +6,8 @@ from geometry_msgs.msg import TransformStamped
 from multi_task_il.datasets.savers import Trajectory
 import numpy as np
 import math
-import os, time
+import os
+import time
 import pickle as pkl
 from camera_controller.srv import *
 from ur5e_2f_85_controller.msg import *
@@ -15,20 +16,21 @@ from cv_bridge import CvBridge
 from ur5e_2f_85_controller.srv import GoToJoint, GoToJointRequest
 import cv2
 
+
 class TrajectoryCollector():
 
     # Obs keys
-    IMAGE='image'
-    JOINT_POS='joint_pos'
-    JOINT_VEL='joint_vel'
-    EEF_POS='eef_pos'
-    EEF_QUAT='eef_quat'
-    GRIPPER_QPOS='gripper_qpos'
-    GRIPPER_QVEL='gripper_qvel'
-    EE_AA='ee_aa'
+    IMAGE = 'image'
+    JOINT_POS = 'joint_pos'
+    JOINT_VEL = 'joint_vel'
+    EEF_POS = 'eef_pos'
+    EEF_QUAT = 'eef_quat'
+    GRIPPER_QPOS = 'gripper_qpos'
+    GRIPPER_QVEL = 'gripper_qvel'
+    EE_AA = 'ee_aa'
 
     def __init__(self, env_camera_name: list, env_camera_topic_name: str, gripper_state_topic: str, joint_state_topic: str, trj_state_topic: str, tcp_frame_name: str, frame_rate: int, saving_folder: str, task_name: str, task_id: str, home_pos: list):
-        
+
         # Set rostopic name parameters
         self._gripper_state_topic = gripper_state_topic
         self._joint_state_topic = joint_state_topic
@@ -40,11 +42,13 @@ class TrajectoryCollector():
         # moveit service
         self._home_pos = home_pos
         rospy.loginfo(self._home_pos)
-        self._moveit_service_client = rospy.ServiceProxy("/go_to_joint", GoToJoint, persistent=True)
+        self._moveit_service_client = rospy.ServiceProxy(
+            "/go_to_joint", GoToJoint, persistent=True)
 
         # camera client service
         rospy.loginfo("---- Waiting for env camera service ----")
-        self._env_camera_service_client = rospy.ServiceProxy(env_camera_topic_name, GetFrames, True)
+        self._env_camera_service_client = rospy.ServiceProxy(
+            env_camera_topic_name, GetFrames, True)
         self._bridge = CvBridge()
         self._show_image = False
         self._env_camera_name = env_camera_name
@@ -52,13 +56,14 @@ class TrajectoryCollector():
         # Init tf listener for the TCP Pose
         self._tfBuffer = tf2_ros.Buffer()
         self._listener = tf2_ros.TransformListener(self._tfBuffer)
-        exception=True
+        exception = True
         while exception:
             try:
                 # Get TCP Pose
-                tcp_pose = self._tfBuffer.lookup_transform('base_link', self._tcp_frame_name, rospy.Time())
+                tcp_pose = self._tfBuffer.lookup_transform(
+                    'base_link', self._tcp_frame_name, rospy.Time())
                 rospy.logdebug(f"TCP Pose {tcp_pose}")
-                exception=False
+                exception = False
             except ((tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException)) as e:
                 exception = True
                 rospy.logerr(e)
@@ -67,36 +72,41 @@ class TrajectoryCollector():
         self._trajectory_cnt = 0
         self._saving_folder = saving_folder
         self._task_name = task_name
-        self._task_id =  '{:02}'.format(task_id)
+        self._task_id = '{:02}'.format(task_id)
 
         try:
             rospy.loginfo(f"Running Task {task_name} - ID {self._task_id}")
-           
-            self._saving_folder = os.path.join(self._saving_folder, task_name, f"task_{self._task_id}")
+
+            self._saving_folder = os.path.join(
+                self._saving_folder, task_name, f"task_{self._task_id}")
             os.makedirs(self._saving_folder)
         except Exception as e:
             print(e)
 
-
-
     def _save_step(self, env_frames: GetFramesResponse, tcp_pose: TransformStamped, joint_state: JointState, state: str, gripper_action: int, gripper_q_pos: int, done: int, reward: int):
 
-        # fill observations        
+        # fill observations
         obs = dict()
-        obs[TrajectoryCollector.EEF_POS]=np.array([tcp_pose.transform.translation.x, tcp_pose.transform.translation.y, tcp_pose.transform.translation.z])
-        obs[TrajectoryCollector.EEF_QUAT]=np.array([tcp_pose.transform.rotation.x, tcp_pose.transform.rotation.y, tcp_pose.transform.rotation.z, tcp_pose.transform.rotation.w])
-        obs[TrajectoryCollector.JOINT_POS]=np.array(joint_state.position)
-        obs[TrajectoryCollector.JOINT_VEL]=np.array(joint_state.velocity)
-        obs[TrajectoryCollector.EE_AA]=self._quat2axisangle(obs[TrajectoryCollector.EEF_QUAT])
-        obs[TrajectoryCollector.GRIPPER_QPOS]=gripper_q_pos
+        obs[TrajectoryCollector.EEF_POS] = np.array(
+            [tcp_pose.transform.translation.x, tcp_pose.transform.translation.y, tcp_pose.transform.translation.z])
+        obs[TrajectoryCollector.EEF_QUAT] = np.array(
+            [tcp_pose.transform.rotation.x, tcp_pose.transform.rotation.y, tcp_pose.transform.rotation.z, tcp_pose.transform.rotation.w])
+        obs[TrajectoryCollector.JOINT_POS] = np.array(joint_state.position)
+        obs[TrajectoryCollector.JOINT_VEL] = np.array(joint_state.velocity)
+        obs[TrajectoryCollector.EE_AA] = self._quat2axisangle(
+            obs[TrajectoryCollector.EEF_QUAT])
+        obs[TrajectoryCollector.GRIPPER_QPOS] = gripper_q_pos
 
         # take env_frames
-        for j, (color_msg, depth_msg) in enumerate(zip(env_frames.color_frames,env_frames.depth_frames)):
-            color_cv_image = self._bridge.imgmsg_to_cv2(color_msg, desired_encoding='bgr8')
-            depth_cv_image = self._bridge.imgmsg_to_cv2(depth_msg, desired_encoding='mono16')
+        for j, (color_msg, depth_msg) in enumerate(zip(env_frames.color_frames, env_frames.depth_frames)):
+            color_cv_image = self._bridge.imgmsg_to_cv2(
+                color_msg, desired_encoding='rgba8')
+            color_cv_image = cv2.cvtColor(color_cv_image, cv2.COLOR_RGBA2RGB)
+            depth_cv_image = self._bridge.imgmsg_to_cv2(
+                depth_msg, desired_encoding='passthrough')
             if self._show_image and self._env_camera_name[j] == 'camera_front':
                 cv2.imshow("Color image", color_cv_image)
-                #cv2.imshow("Depth image", depth_cv_image)
+                # cv2.imshow("Depth image", depth_cv_image)
                 cv2.waitKey(1000)
                 cv2.destroyAllWindows()
             obs[f"{self._env_camera_name[j]}_image"] = color_cv_image
@@ -107,34 +117,36 @@ class TrajectoryCollector():
         info = {'status': state}
 
         # create action
-        action = np.array([tcp_pose.transform.translation.x, 
-                        tcp_pose.transform.translation.y, 
-                        tcp_pose.transform.translation.z, 
-                        tcp_pose.transform.rotation.x, 
-                        tcp_pose.transform.rotation.y, 
-                        tcp_pose.transform.rotation.z, 
-                        tcp_pose.transform.rotation.w, 
-                        gripper_action])
+        action = np.array([tcp_pose.transform.translation.x,
+                           tcp_pose.transform.translation.y,
+                           tcp_pose.transform.translation.z,
+                           tcp_pose.transform.rotation.x,
+                           tcp_pose.transform.rotation.y,
+                           tcp_pose.transform.rotation.z,
+                           tcp_pose.transform.rotation.w,
+                           gripper_action])
 
-        self._trajectory.append(obs=obs, reward=reward, done=done, info=info, action=action)
+        self._trajectory.append(obs=obs, reward=reward,
+                                done=done, info=info, action=action)
 
         self._step += 1
 
         # if the task is completed save the trajectory
-        if done==1:
+        if done == 1:
             rospy.loginfo("Saving Trajectory.....")
             self._save_trajectory()
             return True
-        
+
         return False
-    
+
     def _save_trajectory(self):
-        file_name = os.path.join(self._saving_folder, 'traj{:03d}.pkl'.format(self._trajectory_cnt))
+        file_name = os.path.join(
+            self._saving_folder, 'traj{:03d}.pkl'.format(self._trajectory_cnt))
         self._trajectory_cnt += 1
         pkl.dump({
-            'traj': self._trajectory, 
+            'traj': self._trajectory,
             'len': len(self._trajectory),
-            'env_type': self._task_name, 
+            'env_type': self._task_name,
             'task_id': self._task_id}, open(file_name, 'wb'))
         rospy.loginfo(f"Saved file {file_name}")
         del self._trajectory
@@ -174,53 +186,58 @@ class TrajectoryCollector():
         home_joint_pos_req.joint_goal_pos = np.array(self._home_pos)
         result = self._moveit_service_client.call(home_joint_pos_req)
         if result.success:
-            rospy.loginfo_once("Robot in home position, ready to get a new trajectory")
+            rospy.loginfo_once(
+                "Robot in home position, ready to get a new trajectory")
 
     def get_message(self):
-        exception=True
+        exception = True
         while exception:
             try:
                 # Get TCP Pose
-                tcp_pose = self._tfBuffer.lookup_transform('base_link', self._tcp_frame_name, rospy.Time())
-                exception=False
+                tcp_pose = self._tfBuffer.lookup_transform(
+                    'base_link', self._tcp_frame_name, rospy.Time())
+                exception = False
             except ((tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException)) as e:
                 exception = True
                 rospy.logerr(e)
 
         # Get joint states
-        joint_state = rospy.wait_for_message(topic=self._joint_state_topic, topic_type=JointState)
-        
+        joint_state = rospy.wait_for_message(
+            topic=self._joint_state_topic, topic_type=JointState)
+
         # Get env_frames
         env_frames = self._env_camera_service_client()
-        
+
         # Get gripper state
-        gripper_state = rospy.wait_for_message(topic=self._gripper_state_topic, topic_type=GripperState)
+        gripper_state = rospy.wait_for_message(
+            topic=self._gripper_state_topic, topic_type=GripperState)
 
         # Get the trajectory state
-        trajectory_state = rospy.wait_for_message(topic=self._trj_state_topic, topic_type=TrajectoryState)
+        trajectory_state = rospy.wait_for_message(
+            topic=self._trj_state_topic, topic_type=TrajectoryState)
 
         # Compute the done signal
         done = trajectory_state.trajectory_state == TrajectoryState.TRAJECTORY_END
 
         # Compute reward
         reward = 1 if trajectory_state.trajectory_state == TrajectoryState.TRAJECTORY_END else 0
-        
+
         # Save step if trajectory state is not in idle
         if trajectory_state.trajectory_state != TrajectoryState.TRAJECTORY_IDLE:
             rospy.logdebug(f"Saving step {self._step}")
             return self._save_step(env_frames=env_frames,
-                            tcp_pose=tcp_pose, 
-                            joint_state=joint_state, 
-                            state=trajectory_state.trajectory_state,
-                            gripper_action=1-gripper_state.gripper_open,
-                            gripper_q_pos=gripper_state.finger_position,
-                            done=done,
-                            reward=reward)
+                                   tcp_pose=tcp_pose,
+                                   joint_state=joint_state,
+                                   state=trajectory_state.trajectory_state,
+                                   gripper_action=1-gripper_state.gripper_open,
+                                   gripper_q_pos=gripper_state.finger_position,
+                                   done=done,
+                                   reward=reward)
         rospy.loginfo("Teleoperator in idle mode")
         return False
 
     def run(self):
-        
+
         while not rospy.is_shutdown():
             # press enter to start the collection of a new trajectory
             enter = None
@@ -229,8 +246,8 @@ class TrajectoryCollector():
                 enter = input()
 
             self.init_trj()
-            rospy.loginfo(f"Collecting task {self._task_name} - Sub task {self._task_id} - Counter {self._trajectory_cnt}")
+            rospy.loginfo(
+                f"Collecting task {self._task_name} - Sub task {self._task_id} - Counter {self._trajectory_cnt}")
             trajectory_completed = False
             while not trajectory_completed:
                 trajectory_completed = self.get_message()
-            
